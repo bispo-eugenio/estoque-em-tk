f7cc87b399360ff395cc06861a34f42f6b762ca8
bispo-eugenio <bispoeugenio270@gmail.com>
2025-08-21 11:53:14 -0300

    refactor: troca o master para root + configuração de grid

--- src/project/components/login.py
+++ src/project/components/login.py
@@ -1,6 +1,7 @@
 from login_components.login_root import LoginRoot
 from login_components.login_window import LoginWindow
 from login_components.image_label import ImageLabel
+from tkinter import Label
 from input_data import InputData
 
 
@@ -8,12 +9,12 @@ from input_data import InputData
 def login_acess() -> None:
     root = LoginRoot()
     app = LoginWindow(root)
-    label = ImageLabel(app)
-    username = InputData(app, "Username")
-    password = InputData(app, "Password")
+    label = ImageLabel(app) #Verificar
+    username = InputData(root, "Username")
+    password = InputData(root, "Password")
     label.grid(row=0, column=0)
-    username.grid(row=1, column=0)
-    password.grid(row=3, column=0)
+    username.grid(row=2, column=0)
+    password.grid(row=4, column=0)
 
     app.mainloop()  
 
